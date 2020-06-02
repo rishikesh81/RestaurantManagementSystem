fc43d20d7cb2dd4f50378575c43bd6927413a18d
rajeshrevanwar <rajeshrevanwar@gmail.com>
2020-06-02 20:46:50 +0530

    Intial commit

---
+++ src/test/java/com/restaurant/management/system/RestaurantManagementSystem/RestaurantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.management.system.RestaurantManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
